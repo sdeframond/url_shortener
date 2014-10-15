127cc4fcf244c47d6ba5fdbd73bee259178c8364
Samuel de Framond <samuel@framond.fr>
2014-10-15 18:12:59 +0200

    Implement URL creation through API.

--- app/controllers/api/v1/shortened_urls_controller.rb
+++ app/controllers/api/v1/shortened_urls_controller.rb
@@ -1,10 +1,21 @@
 class Api::V1::ShortenedUrlsController < Api::V1::ApplicationController
   def show
     url = ShortenedUrl.find_by_url_hash!(params[:id])
-    respond_to do |format|
-      format.json {
-        render json: url
-      }
+    respond_with url
+  end
+
+  def create
+    url = ShortenedUrl.new(shortened_url_params)
+    if url.save
+      respond_with url, location: api_v1_shortened_url_url(url)
+    else
+      respond_with url
     end
+  end
+
+  private
+
+  def shortened_url_params
+    params.require(:shortened_url).permit(:full_url)
   end
 end
--- spec/api/v1/shortened_url_spec.rb
+++ spec/api/v1/shortened_url_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "/api/v1/urls", :type => :api do
+  let (:path) {"/api/v1/urls"}
   it "#show" do
     url = create(:shortened_url)
-    get "/api/v1/urls/#{url.url_hash}.json"
+    get "#{path}/#{url.url_hash}.json"
     expect(last_response.status).to eq(200)
     expect(last_response.body).to eq(url.to_json)
+  end
+
+  context "#create" do
+    it "successful" do
+      short_url = build(:shortened_url)
+      post "#{path}.json", shortened_url: short_url.as_json
+      expect(last_response.status).to eq(201)
+    end
+    it 'unsuccessful' do
+      post "#{path}.json", shortened_url: {full_url: nil}
+      expect(last_response.status).to eq(422)
+    end
   end
 end
