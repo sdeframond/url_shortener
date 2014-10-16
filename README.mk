f0cf68901fca2baf771b4e37a5ba19f68fa06646
Samuel de Framond <samuel@framond.fr>
2014-10-16 08:27:27 +0200

    Add a todo list.

---
+++ README.mk
@@ -0,0 +1,5 @@
+## TODO
+
+  - [] Test API for visits.
+  - [] Fix: url_hash can be doubles. We want them unique.
+  - [] Make finding a device by fingerprint smarter. (ie. find it only if the last IP was the same.)
